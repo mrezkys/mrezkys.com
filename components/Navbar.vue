290d931310c9a433df414d5c86d78649e141bd54
mrezkys <mrezkysulihin@gmail.com>
2021-08-03 20:57:23 +0800

    update navbar ui

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,18 +1,31 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-dark ">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </div>
-    </div>
-  </div>
-</nav>
+<div>
+  <b-navbar toggleable="sm" type="dark" variant="dark" class="navbar p-0 bg-transparent">
+    <b-navbar-brand >
+      <img src="~assets/icons/mrezkyslogo.svg" alt="">
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item class="nav-item" to="/page1">Blog</b-nav-item>
+        <b-nav-item class="nav-item" to="/page1">Projects</b-nav-item>
+        <b-nav-item class="nav-item" to="/page1">About Me</b-nav-item>
+        <b-nav-item class="nav-item" to="/page1">Contact</b-nav-item>
+        <b-nav-item class="nav-item" to="/page1" >Hire Me</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
 </template>
+
+<style scoped>
+  .navbar{
+    padding: 20px 0 !important;
+  }
+
+  .nav-item{
+    color: white;
+    font-size: 18px;
+    font-family: 'Poppins';
+  }
+</style>
