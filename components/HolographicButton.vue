b1e49cc9546e62ea4473d0c511f8ac817948499e
mrezkys <mrezkysulihin@gmail.com>
2021-08-03 20:57:59 +0800

    update HolographicButton ui

--- components/HolographicButton.vue
+++ components/HolographicButton.vue
@@ -7,7 +7,7 @@
 </template>
 
 
-<style>
+<style scoped>
     @media (max-width: 575.98px) {
         .holographic-button-wrapper{
             text-align: center !important;
@@ -24,6 +24,7 @@
         text-align: left;
     }
     .holographic-button{
+        font-family: 'Poppins';
         animation: 1s appears;
         padding: 3px 3px;
         background: conic-gradient(from 176.21deg at 50% 50%, #000000 -24.66deg, #FFFFFF 0.25deg, #000000 50.63deg, #000000 51.97deg, #FFFFFF 88.12deg, #000000 142.5deg, #FFFFFF 196.87deg, #000000 256.87deg, #FFFFFF 300deg, #000000 335.2deg, #000000 335.34deg, #FFFFFF 360.25deg), conic-gradient(from 176.21deg at 50% 50%, #000000 -24.66deg, #FFFFFF 0.25deg, #000000 50.63deg, #000000 51.97deg, #FFFFFF 88.12deg, #000000 142.5deg, #FFFFFF 196.87deg, #000000 256.87deg, #FFFFFF 300deg, #000000 335.2deg, #000000 335.34deg, #FFFFFF 360.25deg), radial-gradient(95.11% 95.11% at 36.64% 4.89%, #2AD0CA 0%, #E1F664 22.92%, #FEB0FE 46.88%, #ABB3FC 68.23%, #5DF7A4 87.5%, #58C4F6 100%);
